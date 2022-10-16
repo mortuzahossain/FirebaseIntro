bab237bae3b80811c6a99882b4a4b6b8fe67af8d
Mortuza Hossain <mortuzahossain1997@gmail.com>
2022-10-16 19:05:12 +0600

    Initial Commit

---
+++ FirebaseIntro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseIntro
+//
+//  Created by mortuza on 16/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseIntro/FirebaseIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseIntroApp.swift
+//  FirebaseIntro
+//
+//  Created by mortuza on 16/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
